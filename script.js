4faf23b6e130068088289f67ca06ff70826e75fe
bananaWeb <a89.volkov@gmail.com>
2018-02-01 20:56:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.js-sayThat'). click(function() {
+		showBubble($('.js-text1').val() + $('.js-text2').val());
+
+	});
+
+	function showBubble(text) {
+		$('.bubble').text(text);
+		$('.bubble').addClass('-visible');
+
+		setTimeout(function() {
+			$('.bubble').removeClass('-visible');
+		}, 2000);
+	}
+});
